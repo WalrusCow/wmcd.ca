ef5675b05ac8cf815c5c290b2dc268b889fb03a0
WalrusCow <willgmcd@gmail.com>
2015-03-12 23:31:57 -0700

    Server

--- python/server.py
+++ python/server.py
@@ -9,12 +9,13 @@ PATH_BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 server = Bottle()
 
+server.mount('/blog', blog.app)
+server.merge(wmcd.app)
+
 @server.get('<path:path>')
 def serveStatic(path):
     ''' Fallback routing to anything under '/static' '''
     return static_file(path, root=os.path.join(PATH_BASE, 'static'))
 
 if __name__ == '__main__':
-    server.mount('/blog', blog.app)
-    server.merge(wmcd.app)
     server.run(hostname='localhost', port=27134)
