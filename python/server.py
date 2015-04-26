7d515f040a02c7b265bf4b4e9bf5fad60a3a6f64
WalrusCow <willgmcd@gmail.com>
2015-04-26 15:56:26 -0700

    Remove wmcd app

--- python/server.py
+++ python/server.py
@@ -3,17 +3,27 @@ import os
 from bottle import Bottle, static_file
 
 import blog.app
-import wmcd.app
 
 PATH_BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 server = Bottle()
 
 blog.app.error_handler = server.error_handler
-wmcd.app.error_handler = server.error_handler
 server.mount('/blog', blog.app)
-server.merge(wmcd.app)
 
+def serve_file(filename):
+    return static_file(filename, root=os.path.join(PATH_BASE, 'static'))
+
+@app.get('/')
+def home():
+    return serve_file('home.html')
+
+@server.get('<path:path>')
+def serve_static(path):
+    ''' Fallback routing to any static files. '''
+    return serve_file(path);
+
+#TODO: Error pages
 @server.error(400)
 def error400(error):
     return 'Invalid request!'
@@ -29,11 +39,6 @@ def error404(error):
 @server.error(500)
 def error500(error):
     return 'Something went wrong. My bad.'
-
-@server.get('<path:path>')
-def serveStatic(path):
-    ''' Fallback routing to anything under '/static' '''
-    return static_file(path, root=os.path.join(PATH_BASE, 'static'))
 
 if __name__ == '__main__':
     server.run(hostname='localhost', port=27134)
--- python/wmcd/__init__.py
+++
@@ -1,1 +0,0 @@
-from wmcd.app import app
