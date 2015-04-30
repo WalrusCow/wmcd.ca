9172b4a3d37e0c6286d89a09db0212908cb1d732
WalrusCow <willgmcd@gmail.com>
2015-04-29 23:05:16 -0700

    Snake colors match

--- static/js/snakeMain.js
+++ static/js/snakeMain.js
@@ -6,6 +6,16 @@ require(['/js/config.js'], function() {
   });
 
   requirejs(['snake/snake'], function(SnakeGame) {
-    new SnakeGame();
+    new SnakeGame({
+      // Colors should match the site eh?
+      snakeOptions: {
+        blockConfig: {
+          fill: '#ff9800'
+        },
+        foodConfig: {
+          fill: '#00bcd4'
+        }
+      }
+    });
   });
 });
