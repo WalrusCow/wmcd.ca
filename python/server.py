cb45b073be64537804b11d0beebc08cfbd170a2f
WalrusCow <willgmcd@gmail.com>
2015-04-29 22:18:53 -0700

    testing server

--- python/server.py
+++ python/server.py
@@ -2,19 +2,19 @@ import os
 
 from bottle import Bottle, static_file
 
-import blog.app
+#import blog.app
 
 PATH_BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 server = Bottle()
 
-blog.app.error_handler = server.error_handler
-server.mount('/blog', blog.app)
+#blog.app.error_handler = server.error_handler
+#server.mount('/blog', blog.app)
 
 def serve_file(filename):
     return static_file(filename, root=os.path.join(PATH_BASE, 'static'))
 
-@app.get('/')
+@server.get('/')
 def home():
     return serve_file('home.html')
 
